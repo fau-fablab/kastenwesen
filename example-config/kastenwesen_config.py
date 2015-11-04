fd07c257160c8d579ae968363de47646e15db98b
sedrubal <sebastian.endres@online.de>
2015-11-04 19:55:34 +0100

    More detailed info in config for example servers

--- example-config/kastenwesen_config.py
+++ example-config/kastenwesen_config.py
@@ -3,27 +3,33 @@
 
 config_containers = []
 
-#######
-# web #
-#######
+#########################################
+# web                                   #
+# ===================================== #
+# A web server listening on port 80     #
+#########################################
 web = DockerContainer(
     "webserver", "./webserver/",
     docker_options="-p 80:80 -v " + os.getcwd() + "/webserver/webroot:/var/www:ro",
     tests={'sleep_before': 2, 'http_urls': ['http://localhost'], 'ports': [80]})
 config_containers.append(web)
 
-#########
-# test1 #
-#########
+#########################################
+# test1                                 #
+# ===================================== #
+# A testserver listening on port 1231   #
+#########################################
 test1 = DockerContainer(
     "test1", "./test1/",
     docker_options="-p 1231:1234",
     tests={'sleep_before': 0, 'ports': [1231]})
 config_containers.append(test1)
 
-#########
-# test2 #
-#########
+#########################################
+# test2                                 #
+# ===================================== #
+# A testserver listening on port 1232   #
+#########################################
 test2 = DockerContainer(
     "test2", "./test2/",
     links=[test1],
@@ -31,9 +37,13 @@ test2 = DockerContainer(
     tests={'sleep_before': 0, 'ports': [1232]})
 config_containers.append(test2)
 
-##########################
-# portforwarder_to_test2 #
-##########################
+#########################################
+# portforwarder_to_test2                #
+# ===================================== #
+# A portforwarder that forwards the     #
+# port of test2 to 1337                 #
+# (like a reverse proxy)                #
+#########################################
 portforwarder_to_test2 = DockerContainer(
     "portforwarder-to-test2", "./portforwarder-to-test2/",
     links=[test2],
