be4c798c17c4f3d61da4ce6e2533cfd65f223ff7
Max Gaukler <development@maxgaukler.de>
2016-01-26 10:18:51 +0100

    License
    closes #8

--- kastenwesen.py
+++ kastenwesen.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-"""kastenwesen: a python tool for managing multiple docker containers
+#
+# kastenwesen: a python tool for managing multiple docker containers
+#
+# Copyright (C) 2016 kastenwesen contributors [see git log]
+# https://github.com/fau-fablab/kastenwesen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+kastenwesen: a python tool for managing multiple docker containers
 
 Usage:
   kastenwesen help
