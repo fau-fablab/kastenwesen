338f42daf4eac0611129d5dc270615ea071335d7
Max Gaukler <development@maxgaukler.de>
2016-02-06 00:31:50 +0100

    WIP

--- cron_status_html.sh
+++ cron_status_html.sh
@@ -13,10 +13,12 @@ OKAY=true
 # generate text output
 cat /var/run/kastenwesen_status/colored | sed -r 's/\x1b[\[0-9;]+m//g' > /var/run/kastenwesen_status/text
 
-
-# send mail to $MAIL_USER if last mail is more than MAIL_MAX_MIN ago
-MAIL_MAX_MIN=30
 MAIL_USER=root
+# send mail to $MAIL_USER if last mail is more than $MAIL_MAX_MIN ago
+MAIL_MAX_MIN=30
+# be quiet for $MAIL_SKIP_AFTER_RUN_MIN after starting kastenwesen operations
+MAIL_SKIP_AFTER_RUN_MIN=2
 SKIP_MAIL=false
-test -e  /var/run/kastenwesen_status/last_mail && echo [[ $(stat -L --format %Y /var/run/kastenwesen_status/last_mail) -gt $(date -d "$MAIL_MAX_MIN minutes ago" +%s) ]] && SKIP_MAIL=true
+test -e  /var/run/kastenwesen_status/last_mail && [[ $(stat -L --format %Y /var/run/kastenwesen_status/last_mail) -gt $(date -d "$MAIL_MAX_MIN minutes ago" +%s) ]] && SKIP_MAIL=true
+test -e  /var/lock/kastenwesen.lock && [[ $(stat -L --format %Y /var/lock/kastenwesen.lock) -gt $(date -d "$MAIL_SKIP_AFTER_RUN_MIN minutes ago" +%s) ]] && SKIP_MAIL=true
 ! $OKAY && ! $SKIP_MAIL && { touch /var/run/kastenwesen_status/last_mail && cat /var/run/kastenwesen_status/text | echo mail -s "`hostname --fqdn` kastenwesen status" $MAIL_USER; }
