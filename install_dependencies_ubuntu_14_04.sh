3241e14969d30d2afef25ca6c9027268b190b4be
sedrubal <sebastian.endres@online.de>
2016-02-06 22:18:46 +0100

    Fixed setup scripts?

--- install_dependencies_ubuntu_14_04.sh
+++ install_dependencies_ubuntu_14_04.sh
@@ -3,4 +3,3 @@ set -x
 # this is run on the VM to install docker and all dependencies of kastenwesen.py
 sudo apt-get -q update
 sudo apt-get -y -q install docker.io apparmor-profiles python-pip aha
-sudo pip install -r requirements.txt
