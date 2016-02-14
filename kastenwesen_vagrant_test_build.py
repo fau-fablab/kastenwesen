7394c4d1d42474101e3bfdc4d40d274db3675e79
Max Gaukler <development@maxgaukler.de>
2016-02-14 16:57:12 +0100

    improve vagrant_build
    - skip 'vagrant up' if possible because it takes a few seconds
    - nicer error message on build failure

--- kastenwesen_vagrant_test_build.py
+++ kastenwesen_vagrant_test_build.py
@@ -23,10 +23,25 @@ import sys
 import subprocess
 import os
 
-def run(cmd):
-    """ Run cmd, given as list of strings, one per argument """
+
+def run(cmd, may_fail=False):
+    """
+    Run command, print info, check return status
+
+    :param list[basestring] cmd:  given as list of strings, one item per argument
+    :param bool may_fail: if ``False`` (default), raise ``subprocess.CalledProcessError`` on failure instead of returning ``False``.
+    :return: ``True`` on success, ``False`` otherwise.
+    :rtype: bool
+    """
     print("Running: " + " ".join(cmd))
-    subprocess.check_call(cmd)
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError as e:
+        if may_fail:
+            return False
+        else:
+            raise e
+    return True
 
 args = sys.argv[1:]
 fast_build = False
@@ -43,12 +58,17 @@ if args:
 if clean_vm:
     run("vagrant halt".split(" "))
     run("vagrant destroy -f".split(" "))
-run("vagrant up".split(" "))
+# call "vagrant up" if VM is not yet running
+if "running" not in str(subprocess.check_output("vagrant status".split(" "))):
+    run("vagrant up".split(" "))
 if fast_build:
     build_arg = []
 else:
     build_arg = ["--no-cache"]
-run([os.path.dirname(os.path.realpath(__file__)) + "/kastenwesen_vagrant_wrapper.py", "rebuild"] + build_arg)
+okay = run([os.path.dirname(os.path.realpath(__file__)) + "/kastenwesen_vagrant_wrapper.py", "rebuild"] + build_arg, may_fail=True)
+if not okay:
+    print("\n ERROR! \n")
+    sys.exit(1)
 if not fast_build:
     run("vagrant halt".split(" "))
 print("\nRebuild successful :-)\n")
