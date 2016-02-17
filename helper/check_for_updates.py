caa086b7d0ceeeaead2713f9f64dad9e17178cbf
Max Gaukler <development@maxgaukler.de>
2016-02-17 20:59:25 +0100

    FIX check_for_updates

--- helper/check_for_updates.py
+++ helper/check_for_updates.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 import apt
 import sys
+
+# apt-get update
 cache = apt.Cache()
 try:
     cache.update()
 except apt.cache.LockFailedException:
     sys.stderr.write("Failed to get lock for apt-get update. Are you root?\n")
     sys.exit(1)
-# this only simulates a dist-upgrade:
+
+# reload cache
+cache.close()
+cache.open()
+
+# apt-get dist-upgrade --dry-run
 cache.upgrade(dist_upgrade=True)
 changes = cache.get_changes()
 if changes:
