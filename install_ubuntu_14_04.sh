593e6b3128648a07452fcf92032d3635b32c9dae
Max Gaukler <development@maxgaukler.de>
2016-02-21 00:25:45 +0100

    add initscripts, currently only for ubuntu upstart (#14)

--- install_ubuntu_14_04.sh
+++ install_ubuntu_14_04.sh
@@ -8,4 +8,7 @@ ln -s `pwd`/ /opt/kastenwesen
 ln -s `pwd`/cron.d_kastenwesen /etc/cron.d/kastenwesen
 # This workaround is for testing on VirtualBox shared folders, where the files will be owned by someone else than root and then cron refuses to run
 test -O /etc/cron.d/kastenwesen || { echo "Warning: kastenwesen dir not owned by root, working around this for cronjob"; rm /etc/cron.d/kastenwesen; cp `pwd`/cron.d_kastenwesen /etc/cron.d/kastenwesen; }
+# if using upstart, copy appropriate init files
+initctl --version 2>/dev/null && initctl --version 2>&1 | grep -q upstart && cp init/upstart/* /etc/init/
+# TODO systemd and sysvinit files
 service cron reload
