36c0beb7e30f06882e923165a7ba25a6d0aeb316
Max Gaukler <development@maxgaukler.de>
2016-02-20 21:07:39 +0100

    status cronjob: don't send emails until 10min after last kastenwesen start

--- cron_status_html.sh
+++ cron_status_html.sh
@@ -17,7 +17,10 @@ MAIL_USER=root
 # send mail to $MAIL_USER if last mail is more than $MAIL_MAX_MIN ago
 MAIL_MAX_MIN=30
 # be quiet for $MAIL_SKIP_AFTER_RUN_MIN after starting kastenwesen operations
-MAIL_SKIP_AFTER_RUN_MIN=2
+# since we currently only compare to the time kastenwesen was started,
+# this needs to be a little larger to also work for huge operations like `kastenwesen rebuild --no-cache`
+MAIL_SKIP_AFTER_RUN_MIN=10
+
 SKIP_MAIL=false
 test -e  /var/run/kastenwesen_status/last_mail && [[ $(stat -L --format %Y /var/run/kastenwesen_status/last_mail) -gt $(date -d "$MAIL_MAX_MIN minutes ago" +%s) ]] && SKIP_MAIL=true
 test -e  /var/lock/kastenwesen.lock && [[ $(stat -L --format %Y /var/lock/kastenwesen.lock) -gt $(date -d "$MAIL_SKIP_AFTER_RUN_MIN minutes ago" +%s) ]] && SKIP_MAIL=true
